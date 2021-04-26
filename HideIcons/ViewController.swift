ccb1445a739d327bcb01bf300f50cd01c40bb86c
G.J. Parker <dim@parker9.com>
2021-04-25 18:44:13 -0700

    Initial Commit

---
+++ HideIcons/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HideIcons
+//
+//  Created by parker on 4/25/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
