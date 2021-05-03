53eb2f826cd41764ee177070a9dfb92e5590fa9b
G.J. Parker <dim@parker9.com>
2021-05-03 00:49:06 -0700

    comment out print

--- HideIcons/Hider.swift
+++ HideIcons/Hider.swift
@@ -17,9 +17,8 @@ class MyWindow : NSWindow {
     var cgID: CGWindowID = 0
     var name: String = ""
     var showing: Bool = false
-    func setWin(imageView: NSImageView, name: String, showing: Bool, hidden: Bool) {
+    func setWin(imageView: NSImageView, showing: Bool, hidden: Bool) {
         self.contentView = imageView
-        self.name = name
         self.showing = true
         if showing {
             self.orderFront(nil)
@@ -52,20 +51,17 @@ class Hider {
         if hidden {  // appears the user want to hide icons
             if myScreen.isEmpty { // make windows for all of the Desktops on each screen
                 makeWindows()
-                
                 // get notified when ...
-                NSWorkspace.shared.notificationCenter.addObserver(self, selector: #selector(self.spaceChange), name: NSWorkspace.activeSpaceDidChangeNotification, object: nil) // Space changes
-                NotificationCenter.default.addObserver(self, selector: #selector(self.spaceChange), name: NSApplication.didChangeScreenParametersNotification, object: nil) // Screens change
-                NotificationCenter.default.addObserver(self, selector: #selector(self.spaceChange), name: .spaceChange, object: nil)  // user wants to toggle (via menu or shortcut)
+                NSWorkspace.shared.notificationCenter.addObserver(self, selector: #selector(self.makeWindows(_:)), name: NSWorkspace.activeSpaceDidChangeNotification, object: nil) // Space changes
+                NotificationCenter.default.addObserver(self, selector: #selector(self.makeWindows(_:)), name: NSApplication.didChangeScreenParametersNotification, object: nil) // Screens change
+                NotificationCenter.default.addObserver(self, selector: #selector(self.makeWindows(_:)), name: .spaceChange, object: nil)  // user wants to toggle (via menu or shortcut)
             } else { // not first time, just show windows and update
                 for (_, wins) in myScreen {
                     for win in wins { win.orderBack(nil) }
                 }
-                spaceChange()
+                makeWindows()
             }
-            print("doHider:\(myScreen.count)")
-
-            BGTimer = Timer.scheduledTimer(timeInterval: TimeInterval(60.0), target: self, selector: #selector(self.spaceChange), userInfo: nil, repeats: true)  // this is a lazy capture if the desktop pictures vary w/ time
+            BGTimer = Timer.scheduledTimer(timeInterval: TimeInterval(60.0), target: self, selector: #selector(self.makeWindows(_:)), userInfo: nil, repeats: true)  // this is a lazy capture if the desktop pictures vary w/ time
         } else {
             // stop timer
             BGTimer?.invalidate()
@@ -75,55 +71,40 @@ class Hider {
         }
     }
     
-    @objc func spaceChange() {
-        makeWindows()
-    }
-    
-    func makeWindows(_ option: CGWindowListOption = .optionAll){  // for each desktop we find, take a picture add it onto an array and return it
+    @objc func makeWindows(_ option: CGWindowListOption = .optionAll){  // make window for each desktop = Screens * Spaces
         
         // need to find the Desktop window...
-        //    go through all windows that are on screen
+        //    go through all windows (including those not in this Space
         for window in CGWindowListCopyWindowInfo(option, kCGNullWindowID) as! [[ String : Any]] {
 
             // we need window owned by Dock
             guard let owner = window["kCGWindowOwnerName"] as? String else {continue}
             if owner != "Dock" { continue }
-            // we need window named like "Desktop Picture %"
+            // we need window named like "Desktop Picture"
             guard let name = window["kCGWindowName"] as? String else {continue}
             if !name.hasPrefix("Desktop Picture") { continue }
             // ok, this belongs to a screen. grab the CGWindowID (invariant)
             let index = window["kCGWindowNumber"] as! CGWindowID
-            
             // grab the screen's worth picture CGImag?
             guard let cgImage = CGWindowListCreateImage(.null, .optionIncludingWindow, index, .nominalResolution) else { continue }  // don't do .infinity, freaks out early versions of macOS
-            
-            // so, owned by Dock and has name starting w/ "Desktop Picture"
             let imageView = NSImageView(image: NSImage(cgImage: cgImage, size: NSZeroSize))
             let showing = window["kCGWindowIsOnscreen"] as? Bool ?? false // is it in the active Space?
             
-            for screen in NSScreen.screens { // loop over screens
-                
+            for screen in NSScreen.screens { // loop over screens to find where these pictures go...
                 // find window for this screen
                 let cWin = myScreen[screen]?.filter({$0.cgID == index}).first
-                if cWin != nil {
-                    print("found a window!")
-                    cWin?.setWin(imageView: imageView, name: name, showing: showing, hidden: hidden)
+                if cWin != nil {  // window exists, just update image and settings
+                    cWin?.setWin(imageView: imageView, showing: showing, hidden: hidden)
                 } else {  // new window for this screen
-                    print("need to create a window!")
+                    //print("need to create a window!")
                     let win = createWin(CGRect.init(dictionaryRepresentation: window["kCGWindowBounds"] as! CFDictionary)!)
-                    win.cgID = index
-                    win.setWin(imageView: imageView, name: name, showing: showing, hidden: hidden)
-                    if myScreen[screen] == nil {  myScreen[screen] = [win] } else { myScreen[screen]!.append(win) }
-                    if #available(macOS 10.15, *) {
-                        print("screen:\(screen.localizedName), name:\(win.name), winID:\(win.cgID),rect:\(win.frame),onScreen:\(win.showing)")
-                    } else {
-                        print("screen:\(screen), name:\(win.name), winID:\(win.cgID),rect:\(win.frame),onScreen:\(win.showing)")
-                    }
+                    win.cgID = index  // remember the CGWindowID
+                    win.setWin(imageView: imageView, showing: showing, hidden: hidden)
+                    if myScreen[screen] == nil {  myScreen[screen] = [win] } else { myScreen[screen]!.append(win) } //?
                 }
             }
         }
-        // return the array of windows w/ all Desktop picture(s)
-        print("number of nscreens:\(myScreen.count), desktops:\(myScreen.mapValues({$0.count}))")
+        //print("number of nscreens:\(myScreen.count), desktops:\(myScreen.mapValues({$0.count}))")
         return
     }
     
